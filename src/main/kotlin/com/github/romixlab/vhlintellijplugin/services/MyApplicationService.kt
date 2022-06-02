6c6511ea92a4719321127243fce63480e4e75235
GitHub Action <action@github.com>
2022-06-02 19:43:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/romixlab/vhlintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.romixlab.vhlintellijplugin.services
+
+import com.github.romixlab.vhlintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
